451eb93f4493ce8a82b2eac3dbbcfa344f5afca6
Daniel Stuessy <danielstuessy@gmail.com>
2024-02-14 18:55:49 +0000

    docker setup with auto reload on source change

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, fmt.Sprintf("Hello, world! %v", time.Now().Format("2006-01-02 15:04:05")))
+}
+
 func main() {
-	println("Hello, World!")
+	http.HandleFunc("/", IndexHandler)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
