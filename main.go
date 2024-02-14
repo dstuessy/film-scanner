95bcc4ce8888b789886406fa36faaf1c34b4d3bf
Daniel Stuessy <danielstuessy@gmail.com>
2024-02-14 19:58:21 +0000

    initial template based rendering

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
+	"log"
 	"net/http"
-	"time"
 )
 
+const WebDir = "web"
+
+func ParsePage(name string, data interface{}) (*template.Template, error) {
+	layoutTmpl, err := template.ParseFiles(fmt.Sprintf("%s/layout/master.html", WebDir))
+	if err != nil {
+		return nil, err
+	}
+	path := fmt.Sprintf("%s/pages/%s.html", WebDir, name)
+	pageTmpl, err := template.Must(layoutTmpl.Clone()).ParseFiles(path)
+	if err != nil {
+		return nil, err
+	}
+	return pageTmpl, nil
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, fmt.Sprintf("Hello, world! %v", time.Now().Format("2006-01-02 15:04:05")))
+	indexTmpl, err := ParsePage("index", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := indexTmpl.Execute(w, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
