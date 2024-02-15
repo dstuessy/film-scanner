56f037ca4420abc59774c01cc9fadd1f3863263d
Daniel Stuessy <danielstuessy@gmail.com>
2024-02-15 20:44:07 +0000

    roudimentary page routing and templating

--- main.go
+++ main.go
@@ -3,37 +3,64 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
+	"path/filepath"
+	"strings"
 )
 
-const WebDir = "web"
+const webDir = "web"
+const pageDir = "web/pages"
 
-func ParsePage(name string, data interface{}) (*template.Template, error) {
-	layoutTmpl, err := template.ParseFiles(fmt.Sprintf("%s/layout/master.html", WebDir))
+func parsePage(path string, data interface{}) (*template.Template, error) {
+	layoutTmpl, err := template.ParseFiles(fmt.Sprintf("%s/layout/master.html", webDir))
 	if err != nil {
 		return nil, err
 	}
-	path := fmt.Sprintf("%s/pages/%s.html", WebDir, name)
+
 	pageTmpl, err := template.Must(layoutTmpl.Clone()).ParseFiles(path)
 	if err != nil {
 		return nil, err
 	}
+
 	return pageTmpl, nil
 }
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	indexTmpl, err := ParsePage("index", nil)
+func createPageHandler(filePath string, _ fs.FileInfo, err error) error {
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	if err := indexTmpl.Execute(w, nil); err != nil {
-		log.Fatal(err)
+
+	isHtmlPage := strings.HasSuffix(filePath, ".html")
+	routePath := strings.TrimSuffix(filePath, ".html")
+	routePath = strings.TrimPrefix(routePath, pageDir)
+	routePath = strings.TrimSuffix(routePath, "index")
+
+	fmt.Println("routePath", routePath, filePath)
+
+	if isHtmlPage {
+		http.HandleFunc(routePath, func(w http.ResponseWriter, r *http.Request) {
+			tmpl, err := parsePage(filePath, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if err := tmpl.Execute(w, nil); err != nil {
+				log.Fatal(err)
+			}
+		})
 	}
+
+	return nil
 }
 
 func main() {
-	http.HandleFunc("/", IndexHandler)
+
+	if err := filepath.Walk(pageDir, createPageHandler); err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
