6f7f9d054a66b0daffc91013ab4f7a1f02c6c0a4
Daniel Stuessy <danielstuessy@gmail.com>
2024-02-16 12:16:27 +0000

    feat: google authentication

--- main.go
+++ main.go
@@ -1,64 +1,115 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"html/template"
-	"io/fs"
 	"log"
 	"net/http"
-	"path/filepath"
-	"strings"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
-const webDir = "web"
+const layoutDir = "web/layout"
 const pageDir = "web/pages"
 
-func parsePage(path string, data interface{}) (*template.Template, error) {
-	layoutTmpl, err := template.ParseFiles(fmt.Sprintf("%s/layout/master.html", webDir))
-	if err != nil {
-		return nil, err
-	}
+const accessTokenCookieName = "access_token"
 
-	pageTmpl, err := template.Must(layoutTmpl.Clone()).ParseFiles(path)
-	if err != nil {
-		return nil, err
-	}
-
-	return pageTmpl, nil
-}
-
-func createPageHandler(filePath string, _ fs.FileInfo, err error) error {
+func renderPage(w http.ResponseWriter, path string, data interface{}) error {
+	layoutTmpl, err := template.ParseFiles(fmt.Sprintf("%s/master.html", layoutDir))
 	if err != nil {
 		return err
 	}
 
-	isHtmlPage := strings.HasSuffix(filePath, ".html")
-	routePath := strings.TrimSuffix(filePath, ".html")
-	routePath = strings.TrimPrefix(routePath, pageDir)
-	routePath = strings.TrimSuffix(routePath, "index")
-
-	fmt.Println("routePath", routePath, filePath)
-
-	if isHtmlPage {
-		http.HandleFunc(routePath, func(w http.ResponseWriter, r *http.Request) {
-			tmpl, err := parsePage(filePath, nil)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if err := tmpl.Execute(w, nil); err != nil {
-				log.Fatal(err)
-			}
-		})
+	pageTmpl, err := template.Must(layoutTmpl.Clone()).ParseFiles(fmt.Sprintf("%s%s", pageDir, path))
+	if err != nil {
+		return err
 	}
 
+	if err := pageTmpl.Execute(w, data); err != nil {
+		return err
+	}
 	return nil
 }
 
-func main() {
-	if err := filepath.Walk(pageDir, createPageHandler); err != nil {
-		log.Fatal(err)
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
 	}
+}
+
+func main() {
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("OAUTH_REDIRECT_URL"),
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+			"https://www.googleapis.com/auth/drive.file",
+		},
+		Endpoint: google.Endpoint,
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie(accessTokenCookieName)
+		if err != nil {
+			switch {
+			case errors.Is(err, http.ErrNoCookie):
+				log.Println("Access Token cookie not found")
+				log.Println(err)
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
+			default:
+				log.Println(err)
+				http.Error(w, "server error", http.StatusInternalServerError)
+			}
+		} else {
+			data := struct {
+				AccessToken string
+			}{
+				AccessToken: cookie.Value,
+			}
+
+			if err := renderPage(w, "/index.html", data); err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		data := struct {
+			Url string
+		}{
+			Url: conf.AuthCodeURL("state"),
+		}
+
+		err := renderPage(w, "/login.html", data)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	http.HandleFunc("/oauth2callback", func(w http.ResponseWriter, r *http.Request) {
+		tok, err := conf.Exchange(
+			context.Background(), r.URL.Query().Get("code"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cookie := &http.Cookie{
+			Name:    accessTokenCookieName,
+			Value:   tok.AccessToken,
+			Expires: time.Now().Add(time.Hour * 24),
+		}
+		http.SetCookie(w, cookie)
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
 
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
