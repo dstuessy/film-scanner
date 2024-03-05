07509a681f952c1d8ea3e2ef1f827b11186c7a0f
Daniel Stuessy <danielstuessy@gmail.com>
2024-03-05 22:35:29 +0000

    chore: build into separate tmp folders then deploy from there

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,2 +1,1 @@
-cp build/env.pi tmp/.env
-rsync -rav -e ssh --exclude="*.go" tmp/ danielstuessy@filmscanner.local:/app/
+rsync -rav -e ssh --exclude="*.go" tmp/pi/ danielstuessy@filmscanner.local:/app/
