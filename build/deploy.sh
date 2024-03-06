89c7beb2b8e39b30c9cba63fa08dadce78e95447
Daniel Stuessy <danielstuessy@gmail.com>
2024-03-06 17:59:37 +0000

    chore: raspberry pi systemd service setup files and instructions

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,1 +1,4 @@
-rsync -razP -e ssh --exclude="*.go" tmp/pi/ danielstuessy@filmscanner.local:/app/
+ssh systemd-manager@filmscanner.local "systemctl stop filmscanner.service" && \
+rsync -razP -e ssh --exclude="*.go" tmp/pi/ danielstuessy@filmscanner.local:/app/ && \
+ssh systemd-manager@filmscanner.local "systemctl start filmscanner.service" && \
+ssh systemd-manager@filmscanner.local "systemctl status filmscanner.service"
