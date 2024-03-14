70e48d193e177c403ce4b14957579f9f291a3bd5
Daniel Stuessy <danielstuessy@gmail.com>
2024-03-14 09:14:43 +0000

    feat: adjustable resolution based on env file

--- internal/camera/camera.go
+++ internal/camera/camera.go
@@ -3,6 +3,8 @@ package camera
 import (
 	"errors"
 	"log"
+	"os"
+	"strconv"
 	"time"
 
 	"gocv.io/x/gocv"
@@ -18,6 +20,20 @@ func Open() error {
 	c, err := gocv.OpenVideoCapture(0)
 	if err != nil {
 		return err
+	}
+
+	if os.Getenv("CAM_WIDTH") != "" && os.Getenv("CAM_HEIGHT") != "" {
+		w, err := strconv.ParseFloat(os.Getenv("CAM_WIDTH"), 64)
+		if err != nil {
+			return err
+		}
+		c.Set(gocv.VideoCaptureFrameWidth, w)
+
+		h, err := strconv.ParseFloat(os.Getenv("CAM_HEIGHT"), 64)
+		if err != nil {
+			return err
+		}
+		c.Set(gocv.VideoCaptureFrameHeight, h)
 	}
 
 	webcam = c
